9423cda9a55269beaeb473c19c703009f90e602f
Danial <dmehrabi73@gmail.com>
2023-05-03 11:53:31 +0430

    refactor(resources): change application.properties to application.yml

--- src/main/java/com/smartagilify/projectmanagement/ProjectManagementApplication.java
+++ src/main/java/com/smartagilify/projectmanagement/ProjectManagementApplication.java
@@ -1,13 +1,28 @@
 package com.smartagilify.projectmanagement;
 
+import com.smartagilify.projectmanagement.enumerations.ServerProperties;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.text.MessageFormat;
+
+@RequiredArgsConstructor
 @SpringBootApplication
-public class ProjectManagementApplication {
+public class ProjectManagementApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectManagementApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(ProjectManagementApplication.class);
+    private final ServerProperties properties;
 
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementApplication.class, args);
+    }
+
+    public void run(String... args) {
+        final String env = MessageFormat.format("using environment: [{0}]", properties.getEnvironment());
+        logger.info(env);
+    }
 }
