87979f8c113569d2c75f5aeebe342232d3499002
Danial <dmehrabi73@gmail.com>
2023-06-08 13:40:55 +0430

    refactor(conf-file): fix yml file and environment

--- src/main/java/com/smartagilify/projectmanagement/environments/ServerProperties.java
+++ src/main/java/com/smartagilify/projectmanagement/environments/ServerProperties.java
@@ -2,15 +2,17 @@ package com.smartagilify.projectmanagement.environments;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties
-@ConfigurationProperties(prefix = "app-metadata")
+@ConfigurationProperties(prefix = "application.yml")
 @Getter
 @Setter
 public class ServerProperties {
+    @Value("${spring.profiles.active}")
     private String environment;
 }
